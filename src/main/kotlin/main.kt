e41370753f660b81174d940da0f560f7f5744033
Ignacio <as.ignasi97@gmail.com>
2021-04-09 14:28:36 +0200

    Acabado y bonito

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,33 +5,18 @@ const val tiempoDormir = 1000L
 fun main() {
     runBlocking {
         this.launch {
-            funAsync()
-            println("He terminado funAsync1")
+            repeat (10){
+                println(it+1)
+                delay(tiempoDormir)
+            }
         }
         this.launch {
-            funAsync2()
-            println("He terminado funAsync2")
+            for (it in 1..2){
+                delay(tiempoDormir*5)
+                println("Sigo Vivo")
+            }
         }
     }
-}
-
-private suspend fun funAsync(){
-    val result = GlobalScope.async{
-        repeat (10){
-            println(it+1)
-            delay(tiempoDormir)
-        }
-    }
-    result.await()
-}
-
-private suspend fun funAsync2(){
-    val result = GlobalScope.async{
-        for (it in 1..2){
-            delay(tiempoDormir*5)
-            println("Sigo Vivo")
-        }
-    }
-    result.await()
+    println("He terminado el runBlocking")
 }
 
